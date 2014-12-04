7a2b2f04b43f6d81d5de9223d6c8e9f84b01bead
Steven Roseman <stevierose@fullsail.edu>
2014-12-03 21:56:55 -0500

    added updated files

---
+++ WeatherData/app/src/main/java/rosemak/weatherdata/DetailFragment.java
@@ -0,0 +1,72 @@
+package rosemak.weatherdata;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by stevierose on 12/3/14.
+ */
+public class DetailFragment extends Fragment{
+
+    public static final String TAG = "DetailFragment";
+
+    private static final String ARG_WEATHERNAME = "cityName";
+    private static final String ARG_WEATHERTYPE = "weatherType";
+    private static final String ARG_WEATHERDESCRIPTION = "weatherDescription";
+
+    private String mCityName;
+    private String mWeatherType;
+    private String mWeatherDescription;
+
+    public static DetailFragment newInstance(String cityName, String weatherType, String weatherDescription) {
+        DetailFragment frag = new DetailFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_WEATHERNAME, cityName);
+        args.putSerializable(ARG_WEATHERTYPE, weatherType);
+        args.putSerializable(ARG_WEATHERDESCRIPTION, weatherDescription);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    public DetailFragment() {
+         mCityName = "";
+        mWeatherType = "";
+       mWeatherDescription = "";
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getArguments()!= null) {
+            mCityName = (String) getArguments().getSerializable(ARG_WEATHERNAME);
+            mWeatherType = (String) getArguments().getSerializable(ARG_WEATHERTYPE);
+            mWeatherDescription = (String) getArguments().getSerializable(ARG_WEATHERDESCRIPTION);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       View detailView = inflater.inflate(R.layout.detail_fragment, container, false);
+        return detailView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        TextView cityName = (TextView) getView().findViewById(R.id.cityNameTextView);
+        cityName.setText(mCityName);
+
+        TextView weatherType = (TextView) getView().findViewById(R.id.weatherType);
+        weatherType.setText(mWeatherType);
+
+        TextView weatherDescription = (TextView) getView().findViewById(R.id.weatherDescription);
+        weatherDescription.setText(mWeatherDescription);
+    }
+}
